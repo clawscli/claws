824889064029401ad7b39bd5809d9edeb7ada841
m@yim.jp <m@yim.jp>
2025-12-24 15:52:46 +0000

    fix: normalize costexplorer service name

--- custom/costexplorer/costs/dao.go
+++ custom/costexplorer/costs/dao.go
@@ -154,7 +154,7 @@ func NewCostResource(group types.Group, start, end string) *CostResource {
 	return &CostResource{
 		BaseResource: dao.BaseResource{
 			ID:  serviceName,
-			ARN: fmt.Sprintf("cost-explorer::%s", serviceName),
+			ARN: fmt.Sprintf("costexplorer::%s", serviceName),
 		},
 		ServiceName:   serviceName,
 		Cost:          cost,
